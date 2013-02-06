3a1c687db6a04372b9bb22dfa6a29d7a920f0a6d
cdonahue <christopherdonahue@gmail.com>
2013-02-05 19:54:38 -0600

    Added juce code to src for project.

---
+++ src/gui/Source/PluginProcessor.h
@@ -0,0 +1,73 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a Juce application.
+
+  ==============================================================================
+*/
+
+#ifndef __PLUGINPROCESSOR_H_8B6F22FB__
+#define __PLUGINPROCESSOR_H_8B6F22FB__
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+
+//==============================================================================
+/**
+*/
+class GeneticProgrammingSynthesizerAudioProcessor  : public AudioProcessor
+{
+public:
+    //==============================================================================
+    GeneticProgrammingSynthesizerAudioProcessor();
+    ~GeneticProgrammingSynthesizerAudioProcessor();
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock);
+    void releaseResources();
+
+    void processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages);
+
+    //==============================================================================
+    AudioProcessorEditor* createEditor();
+    bool hasEditor() const;
+
+    //==============================================================================
+    const String getName() const;
+
+    int getNumParameters();
+
+    float getParameter (int index);
+    void setParameter (int index, float newValue);
+
+    const String getParameterName (int index);
+    const String getParameterText (int index);
+
+    const String getInputChannelName (int channelIndex) const;
+    const String getOutputChannelName (int channelIndex) const;
+    bool isInputChannelStereoPair (int index) const;
+    bool isOutputChannelStereoPair (int index) const;
+
+    bool acceptsMidi() const;
+    bool producesMidi() const;
+    bool silenceInProducesSilenceOut() const;
+
+    //==============================================================================
+    int getNumPrograms();
+    int getCurrentProgram();
+    void setCurrentProgram (int index);
+    const String getProgramName (int index);
+    void changeProgramName (int index, const String& newName);
+
+    //==============================================================================
+    void getStateInformation (MemoryBlock& destData);
+    void setStateInformation (const void* data, int sizeInBytes);
+
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GeneticProgrammingSynthesizerAudioProcessor)
+};
+
+#endif  // __PLUGINPROCESSOR_H_8B6F22FB__
