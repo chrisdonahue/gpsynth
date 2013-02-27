aa258c887f45c26987194da523960713e3a14580
cdonahue <christopherdonahue@gmail.com>
2013-02-26 19:32:46 -0600

    Think I fixed the issue but wtf was that

---
+++ Source/Synth/GPNetwork.cpp
@@ -0,0 +1,139 @@
+/*
+  ==============================================================================
+
+    GPNetwork.cpp
+    Created: 6 Feb 2013 11:05:02am
+    Author:  cdonahue
+
+  ==============================================================================
+*/
+
+#include "GPNetwork.h"
+
+/*
+    ============
+    CONSTRUCTION
+    ============
+*/
+
+GPNetwork::GPNetwork(GPNode* r) :
+allNodes()
+{
+    ID = -1;
+    fitness = -1;
+    root = r;
+    depth = -1;
+    asText = "";
+}
+
+GPNetwork::~GPNetwork() {
+    delete root;
+}
+
+GPNetwork* GPNetwork::getCopy() {
+    GPNetwork* copy = new GPNetwork(root->getCopy());
+    copy->traceNetwork();
+    return copy;
+}
+
+/*
+    ===========
+    EXAMINATION
+    ===========
+*/
+
+double GPNetwork::evaluate(double* t, double* v) {
+    return root->evaluate(t, v);
+}
+
+std::string GPNetwork::toString() {
+    return asText;
+}
+
+int GPNetwork::getDepth() {
+    return depth;
+}
+
+GPNode* GPNetwork::getRoot() {
+    return root;
+}
+
+bool GPNetwork::equals(GPNetwork* other) {
+    return false;
+}
+
+/*
+    ========
+    MUTATION
+    ========
+*/
+
+GPNode* GPNetwork::getRandomNetworkNode(GPRandom* r) {
+    if (allNodes.size() == 0) {
+        std::cerr << "Tried to get a random node of an empty or untraced network. This shouldn't happen" << std::endl;
+        return NULL;
+    }
+    return allNodes[r->random(allNodes.size())];
+}
+
+void GPNetwork::mutate(GPNodeParams* p) {
+    asText = "";
+    getRandomNetworkNode(p->rng)->mutate(p);
+}
+
+void GPNetwork::traceNetwork() {
+    allNodes.clear();
+    depth = 0;
+    root->traceSubtree(&allNodes, NULL, &depth, 0);
+    asText = root->toString();
+}
+
+/*
+    This method replaces the subtree rooted at node old with node new's
+*/
+void GPNetwork::replaceSubtree(GPNode* old, GPNode* nu) {
+    // handle root case
+    if (old == root) {
+        root = nu;
+    }
+    else {
+        // replace parent-child links
+        if (old->parent->left == old)
+            old->parent->left = nu;
+        else if (old->parent->right == old)
+            old->parent->right = nu;
+        else
+            std::cerr << "Bad parent-child links detected during subtree replacement." << std::endl;
+    }
+
+    // assign nu pointers
+    nu->parent = old->parent;
+
+    traceNetwork();
+}
+
+/*
+    This method swaps node two into node one's spot in the network tree
+*/
+void GPNetwork::swap(GPNode* old, GPNode* nu) {
+    /*
+    // handle root case
+    if (old == root)
+        root = nu;
+    
+    // replace parent-child links
+    if (old->parent != NULL) {
+        if (old->parent->left == old)
+            old->parent->left = nu;
+        else if (old->parent->right == old)
+            old->parent->right = nu;
+        else
+            std::cerr << "Bad parent-child links detected during node swap." << std::endl;
+    }
+
+    // assign nu pointers
+    nu->parent = old->parent;
+    nu->left = old->left;
+    nu->right = old->right;
+    */
+}
