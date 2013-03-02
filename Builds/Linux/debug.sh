a2f9dbc23a085ff0be01aa40168132181e85aa92
cdonahue <christopherdonahue@gmail.com>
2013-03-01 18:29:05 -0600

    Updated TODO

--- Builds/Linux/debug.sh
+++ Builds/Linux/debug.sh
@@ -9,7 +9,7 @@ set -e
 #gdb --args ./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --popsize 5 --addchance 0.0 --removechance 0.0 --mutatechance 1.0 --crosschance 0.0 --threshold 10 --numgenerations 5 --selection 9 --cross 0 --values 220.0 1.0 1.0
 #gdb --args ./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --thershold 0 --numgenerations 500 --values 220 1 1 --popsize 500 --mid 6 --md 17 --ctype 0 --rselect 0 --cselect 0 --crosspercent 0.9 --addchance 0.0 --removecance 0.0 --mutatechance 0.05
 #gdb --args ./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --threshold 0 --numgenerations 2 --values 220 1 1 --popsize 50 --mid 6 --md 8 --ctype 0 --rselect 0 --cselect 0 --crosspercent 0.9 --addchance 0.0 --removechance 0.0 --mutatechance 0.05
-gdb --args ./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --threshold 0 --numgenerations 2 --values 220 1 1 --popsize 40 --mid 5 --md 8 --ctype 0 --rselect 0 --cselect 0 --crosspercent 0.9 --addchance 0.0 --removechance 0.0 --mutatechance 0.05
+gdb --args ./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --threshold 0 --numgenerations 2 --values 220 1 1 --popsize 50 --mid 4 --md 8 --ctype 0 --rselect 0 --cselect 0 --crosspercent 0.9 --mutatechance 0.0
 
 
 set +e
--- Builds/Linux/run.sh
+++ Builds/Linux/run.sh
@@ -7,6 +7,6 @@ set -e
 
 #./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --popsize 5 --seed 0 --addchance 0.0 --removechance 0.0 --mutatechance 1.0 --crosschance 0.0 --threshold 10 --numgenerations 2 --selection 9 --cross 0 --values 220.0 1.0 1.0
 #./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --popsize 5 --addchance 0.0 --removechance 0.0 --mutatechance 1.0 --crosschance 0.0 --threshold 10 --numgenerations 5 --selection 9 --cross 0 --values 220.0 1.0 1.0
-./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --threshold 0 --numgenerations 100 --values 220 1 --popsize 500 --mid 4 --md 6 --ctype 0 --rselect 0 --cselect 0 --crosspercent 0.9 --mutatechance 0.0
+./build/GeneticProgrammingSynthesizer --target ./build/AMExample.wav --exp 0 --threshold 0 --numgenerations 2 --values 220 1 --popsize 50 --mid 4 --md 6 --ctype 0 --rselect 0 --cselect 0 --crosspercent 0.9 --mutatechance 0.0
 
 set +e
