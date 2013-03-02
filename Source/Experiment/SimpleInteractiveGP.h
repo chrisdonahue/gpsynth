19c0b1120e31efc9451fc6c793eb47750c9014c7
cdonahue <christopherdonahue@gmail.com>
2013-03-02 00:44:48 -0600

    Added simple GUI thing

---
+++ Source/Experiment/SimpleInteractiveGP.h
@@ -0,0 +1,85 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated file created by the Jucer!
+
+  Creation date:  2 Mar 2013 12:42:40am
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Jucer version: 1.12
+
+  ------------------------------------------------------------------------------
+
+  The Jucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright 2004-6 by Raw Material Software ltd.
+
+  ==============================================================================
+*/
+
+#ifndef __JUCER_HEADER_SIMPLEINTERACTIVEGP_SIMPLEINTERACTIVEGP_401095A8__
+#define __JUCER_HEADER_SIMPLEINTERACTIVEGP_SIMPLEINTERACTIVEGP_401095A8__
+
+//[Headers]     -- You can add your own extra header files here --
+#include "juce.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Jucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class SimpleInteractiveGP  : public Component,
+                             public ButtonListener,
+                             public SliderListener
+{
+public:
+    //==============================================================================
+    SimpleInteractiveGP ();
+    ~SimpleInteractiveGP();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g);
+    void resized();
+    void buttonClicked (Button* buttonThatWasClicked);
+    void sliderValueChanged (Slider* sliderThatWasMoved);
+
+
+
+    //==============================================================================
+    juce_UseDebuggingNewOperator
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+    TextButton* playsound;
+    Slider* freqslider;
+    Label* freqlabel;
+    TextButton* upvote;
+    TextButton* downvote;
+    Label* fitnesslabel;
+    Label* fitnessamount;
+    TextButton* savebutton;
+    TextButton* nextbutton;
+
+
+    //==============================================================================
+    // (prevent copy constructor and operator= being generated..)
+    SimpleInteractiveGP (const SimpleInteractiveGP&);
+    const SimpleInteractiveGP& operator= (const SimpleInteractiveGP&);
+};
+
+
+#endif   // __JUCER_HEADER_SIMPLEINTERACTIVEGP_SIMPLEINTERACTIVEGP_401095A8__
