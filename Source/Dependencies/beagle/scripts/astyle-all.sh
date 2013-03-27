c8c05ae349d50e854982c3cabb4b26ee5f46380b
cdonahue <christopherdonahue@gmail.com>
2013-03-27 18:09:35 -0500

    Removed beagle

--- Source/Dependencies/beagle/scripts/astyle-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ALLFILES=`ls ../src/Beagle/EC.hpp ../src/Beagle/EC/*.?pp`
-for F in $ALLFILES; do
-  astyle --style="k&r" --indent=tab=4 --indent-col1-comments --lineend=linux $F
-  rm $F.orig
-done
--- Source/Dependencies/beagle/scripts/astyle-ob.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-exec astyle --style="k&r" --indent=tab=4 --indent-col1-comments --lineend=linux $*
-
