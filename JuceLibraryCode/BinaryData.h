403748b21e0563f3e637b00526e2dafcd6988051
cdonahue <christopherdonahue@gmail.com>
2013-03-27 18:31:46 -0500

    Added binary data stuff

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,46 @@
+/* =========================================================================================
+
+   This is an auto-generated file, created by The Introjucer 3.0.0
+   Do not edit anything in this file!
+
+*/
+
+namespace BinaryData
+{
+    extern const char*   ProductInfo_txt;
+    const int            ProductInfo_txtSize = 131;
+
+    extern const char*   ProductWebsite_html;
+    const int            ProductWebsite_htmlSize = 219;
+
+    extern const char*   SedCommand_txt;
+    const int            SedCommand_txtSize = 61;
+
+    extern const char*   kissfft_hh;
+    const int            kissfft_hhSize = 10920;
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
+
+    //==============================================================================
+    // This class acts as an ImageProvider that will access the BinaryData images
+    class ImageProvider  : public juce::ComponentBuilder::ImageProvider
+    {
+    public:
+        ImageProvider() noexcept {}
+
+        juce::Image getImageForIdentifier (const juce::var& imageIdentifier)
+        {
+            int dataSize = 0;
+            const char* const data = getNamedResource (imageIdentifier.toString().toUTF8(), dataSize);
+
+            if (data != nullptr)
+                return juce::ImageCache::getFromMemory (data, dataSize);
+
+            return juce::Image();
+        }
+
+        juce::var getIdentifierForImage (const juce::Image&)  { return juce::var(); }
+    };
+}
