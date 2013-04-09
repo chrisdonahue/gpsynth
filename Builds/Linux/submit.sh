4814b548c75cfe0187b373942e43c5f0b2a1e3c8
Christopher Donahue <christopherdonahue@gmail.com>
2013-04-09 01:30:41 -0500

    Added some stuff for Condor, changed name of executable. It's running on Condor though\!

---
+++ Builds/Linux/submit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# WARNING: This is not a makefile! It is provided only to give a sense of the commands
+# necessary to build this whole project. 
+
+set -e
+
+export PATH=/lusr/opt/condor/bin:$PATH
+condor_submit results/condor.desc
+
+set +e
