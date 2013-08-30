5caad093ccb3f56c6c84fc17bf0c5ca753e9d010
Christopher Donahue <christopherdonahue@gmail.com>
2013-08-29 19:47:36 -0500

    Comitting condor config stuff

--- Builds/Linux/results/condor_run.sh
+++ Builds/Linux/results/condor_run.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:../../../../Source/Dependencies/PACC/lib:../../../../Source/Dependencies/OpenBEAGLE/lib
+
 ../../build/GPAudioTargetMatchingExperimentCondor\
     --sys_info \
-    --logger_cfg ./cfg/logger.cfg \
-    --synth_cfg ./cfg/synth.cfg \
-    --primitives ./cfg/primitives.cfg \
-    --me_cfg ./cfg/me.cfg \
-    --beagle_cfg ./cfg/beagle.cfg \
+    --logger_cfg ./condor_cfg/logger.cfg \
+    --synth_cfg ./condor_cfg/synth.cfg \
+    --primitives ./condor_cfg/primitives.cfg \
+    --me_cfg ./condor_cfg/me.cfg \
+    --beagle_cfg ./condor_cfg/beagle.cfg \
     --target ../../samples/TrumpetEb5.wav \
     --output_dir ./output/ \
     --seed $1 \
