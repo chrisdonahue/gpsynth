db31d3fa92df1a1b5f163903ea6ca39f45c648e1
Chris Donahue <christopherdonahue@gmail.com>
2013-09-11 18:38:30 -0500

    Added binary data back in

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,18 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+namespace BinaryData
+{
+    extern const char*   kissfft_hh;
+    const int            kissfft_hhSize = 10920;
+
+    extern const char*   GPMutatableParam_h;
+    const int            GPMutatableParam_hSize = 4493;
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes) throw();
+}
